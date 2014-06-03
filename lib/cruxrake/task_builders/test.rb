c21af75f36d9f084689dd194536aedf8a9a6492d
Leo Hernandez <leo@booleansolutions.com>
2014-06-03 18:04:04 -0400

    added options to make nunit runner more verbose... the right way this time

--- lib/cruxrake/task_builders/test.rb
+++ lib/cruxrake/task_builders/test.rb
@@ -11,6 +11,8 @@ module CruxRake
       task = test_runner_task :test => test_dependencies do |tests|
         tests.files = FileList["**/*.Tests/bin/#{configuration}/*.Tests.dll"]
         tests.exe = locate_tool("#{package_dir}/NUnit.Runners.*/tools/nunit-console.exe")
+        tests.parameters.add('/labels')
+        tests.parameters.add('/trace=Verbose')
       end
       task.add_description 'Run unit tests'
     end
