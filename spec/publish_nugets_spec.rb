f4e1243fe2815e7a9f1cff12c63a67065fa9fbf5
Rob Scaduto <rob@thirdwave.it>
2014-11-18 14:24:15 -0500

    Minor refactoring to publsh_nugets_spec

--- spec/publish_nugets_spec.rb
+++ spec/publish_nugets_spec.rb
@@ -1,3 +1,6 @@
+FEED_URL = 'https://www.nuget.org'
+SYMBOLS_FEED_URL = 'http://nuget.gw.symbolsource.org/Public/NuGet'
+
 describe Physique::PublishNugetsConfig do
   let(:config) { Physique::PublishNugetsConfig.new }
   let(:opts) { config.opts }
@@ -6,14 +9,14 @@ describe Physique::PublishNugetsConfig do
     let(:first_feed) { opts.feeds[0] }
 
     it 'should create a default feed and delegate its configuration' do
-      config.feed_url = 'https://www.nuget.org'
-      config.symbols_feed_url = 'http://nuget.gw.symbolsource.org/Public/NuGet'
+      config.feed_url = FEED_URL
+      config.symbols_feed_url = SYMBOLS_FEED_URL
 
       opts.feeds.count.should eq(1)
 
       first_feed.name.should eq('default')
-      first_feed.feed_url.should eq('https://www.nuget.org')
-      first_feed.symbols_feed_url.should eq('http://nuget.gw.symbolsource.org/Public/NuGet')
+      first_feed.feed_url.should eq(FEED_URL)
+      first_feed.symbols_feed_url.should eq(SYMBOLS_FEED_URL)
       first_feed.gen_symbols.should be_true
     end
   end
@@ -25,13 +28,13 @@ describe Physique::PublishNugetsFeedConfig do
 
   it 'should set opts values' do
     config.name = 'default'
-    config.feed_url = 'https://www.nuget.org'
-    config.symbols_feed_url = 'http://nuget.gw.symbolsource.org/Public/NuGet'
+    config.feed_url = FEED_URL
+    config.symbols_feed_url = SYMBOLS_FEED_URL
     config.api_key = 'API_KEY'
 
     feed.name.should eq('default')
-    feed.feed_url.should eq('https://www.nuget.org')
-    feed.symbols_feed_url.should eq('http://nuget.gw.symbolsource.org/Public/NuGet')
+    feed.feed_url.should eq(FEED_URL)
+    feed.symbols_feed_url.should eq(SYMBOLS_FEED_URL)
     feed.gen_symbols.should be_true
   end
 end
