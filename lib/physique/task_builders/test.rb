dcc4fa581d80141de1351e1e29348e56153bdec4
Rob Scaduto <rob@thirdwave.it>
2014-11-18 14:24:54 -0500

    Removed PersistenceTest exclusion from test runner

--- lib/physique/task_builders/test.rb
+++ lib/physique/task_builders/test.rb
@@ -30,7 +30,6 @@ module Physique
         tests.exe = locate_tool("#{package_dir}/NUnit.Runners.*/tools/nunit-console.exe")
         tests.parameters.add('/labels')
         tests.parameters.add('/trace=Verbose')
-        tests.parameters.add('/exclude=PersistenceTest')
       end
       task.add_description 'Run unit tests'
     end
